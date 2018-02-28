1f93169391b9d9a333261c1e3b48915bb38a7612
Jerry Lee <jwl62@georgetown.edu>
2018-02-28 20:21:48 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,11 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(arr)
+  to_return = []
+  arr.each do |name|
+    to_return.push(badge_maker(name))
+  end
+  to_return
+end
